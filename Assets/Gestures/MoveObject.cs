e6f176236ebf1f127954ebb500c1d880cdb971c5
XiaodanDX <daniellexu0809@gmail.com>
2025-02-09 18:06:39 -0500

    Some changes to deflection & grab

--- Assets/Gestures/MoveObject.cs
+++ Assets/Gestures/MoveObject.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 public class MoveObject : MonoBehaviour
 {
     public float speed = 5f;
+    public float lifetime = 6f;
+
+    void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
+
     void Update()
     {
         transform.position += transform.forward * speed * Time.deltaTime;
     }
 }
+
--- Assets/Scripts/MoveBeam.cs
+++ Assets/Scripts/MoveBeam.cs
@@ -6,10 +6,12 @@ public class MoveBeam : MonoBehaviour
 {
     public float speed = 5f;
     private Rigidbody rb;
+    public float lifetime = 6f;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.velocity = transform.forward * speed; // Move using Rigidbody
+        Destroy(gameObject, lifetime);
     }
 }
