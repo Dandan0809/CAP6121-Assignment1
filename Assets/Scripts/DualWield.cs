9e3df43ee7415982ede9b1bd6ef69199c14c2c39
Miguel Rosario <miguelangel53013@gmail.com>
2025-02-10 00:26:17 -0500

    Fixed hand tracking issue.

--- Assets/Scripts/DualWield.cs
+++ Assets/Scripts/DualWield.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class DualWield : MonoBehaviour
 {
 
-    public GameObject secondSaber;
+    public MeshRenderer secondSaber;
+    public Collider saberCollider;
     public GameObject[] abilities;
 
     public void UnleashForce()
     {
-        secondSaber.SetActive(true);
+        secondSaber.enabled = true;
+        saberCollider.enabled = true;
         foreach (GameObject obj in abilities)
         {
             obj.SetActive(false);
